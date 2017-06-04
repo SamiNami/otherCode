bf1ae58d6e2c2d7fa60bdd3c88fabaa6f376aa21
Sami Peralahti <sami.peralahti@gmail.com>
2017-06-04 21:33:01 +0300

    worked on good structure: obj for the DOMstrings,getInput and eventlisteners

--- moreJS/6-Budgety/6-Budgety/app.js
+++ moreJS/6-Budgety/6-Budgety/app.js
@@ -1,37 +1,73 @@
 
+
 let budgetController = (function(){
 
-  let x = 23;
 
-  let add = function(a){
-    return x + a;
-  }
-
-  return {
-    publicTest: function(b){
-      return add(b);
-    }
-  }
 })();
 
 
 let UIController = (function(){
 
+  let DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
 
+  return {
+    getInput: function(){
+      return{
+        // type will be either inc or exp
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: function(){
+      return DOMstrings;
+    }
+
+  };
 
 })();
 
 
 
-
+// Global App Controller
 let controller = (function(budgetCtrl,UICtrl){
 
-  let z = budgetCtrl.publicTest(5);
+  let DOM = UICtrl.getDOMstrings();
 
-  return{
-    anotherPublic: function(){
-      console.log(z);
-    }
+
+  let ctrlAddItem = function(){
+
+    // 1. Get the field input data
+    let input = UICtrl.getInput();
+    console.log(input);
+
+    //2. Add the item to the budget Controller
+
+    //3. Add the item the the UI
+
+    //4. Calculate the budget
+
+    //5. Display the budget on the UI
+
+    console.log("tset");
   }
+
+  document.querySelector(DOM.inputBtn).addEventListener("click",ctrlAddItem);
+
+  document.addEventListener("keypress", function(event){
+
+    if(event.keyCode === 13 || event.which === 13){
+      ctrlAddItem();
+    }
+
+
+  });
+
 
 })(budgetController,UIController);
