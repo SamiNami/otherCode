eced3f4c676db2eb5181646ae3d953eee370fbcd
Sami Peralahti <sami.peralahti@gmail.com>
2017-06-07 18:05:21 +0300

    more arrow functions

--- moreJS/7-EcmaScript2015/script.js
+++ moreJS/7-EcmaScript2015/script.js
@@ -100,7 +100,7 @@ console.log(`${firstName} `.repeat(5));
 */
 
 // Arrow functions
-
+/*
 const years = [1990,1965,1982,1937];
 
 //ES5
@@ -124,3 +124,104 @@ ages6 = years.map((el, index) => {
   return `Age element ${index + 1}: ${age}`
 });
 console.log(ages6);
+
+
+
+years.forEach(el => console.log(el));
+
+years.forEach((el,index) => console.log(`Order of entered: ${index} The year: ${el}`));
+
+years.forEach((el,index) => {
+  const result = el - 1000;
+  console.log(`Element - 1000 = ${result} ps, this is the index: ${index}`);
+});
+*/
+
+
+// Arrow function 2
+
+// ES5
+/*
+var box5 = {
+  color: "green",
+  position: 1,
+  clickMe: function(){
+    //ES5 hack
+    var self = this;
+
+    document.querySelector(".green").addEventListener("click", function(){
+        var str = "This is box number " + self.position + " and it is " + self.color;
+        alert(str);
+    });
+  }
+}
+box5.clickMe();
+*/
+//ES6
+
+const box6 = {
+  color: "green",
+  position: 1,
+  clickMe: function(){
+
+    document.querySelector(".green").addEventListener("click", () =>{
+        var str = "This is box number " + this.position + " and it is " + this.color;
+        alert(str);
+    });
+  }
+}
+//box6.clickMe();
+
+// be careful with arrow fucntions!
+/*
+const box66 = {
+  color: "green",
+  position: 1,
+  clickMe: () =>{
+
+    document.querySelector(".green").addEventListener("click", () =>{
+        var str = "This is box number " + this.position + " and it is " + this.color;
+        alert(str);
+    });
+  }
+}
+box66.clickMe();
+
+
+
+function Person(name){
+  this.name = name;
+}
+*/
+/*
+//ES5
+Person.prototype.myFriends5 = function(friends){
+
+  var arr = friends.map(function(el){
+    return this.name + " is friends with " + el;
+  }.bind(this));
+
+  console.log(arr);
+}
+
+var friends = ["bob","jane","mark"];
+new Person("John").myFriends5(friends);
+
+// ES6
+
+function Person(name){
+  this.name = name;
+}
+
+//ES5
+Person.prototype.myFriends6 = function(friends){
+
+  var arr = friends.map(el => `${this.name} is friends with ${el}`
+    );
+
+  console.log(arr);
+}
+
+let friends = ["bob","jane","mark"];
+new Person("Mike").myFriends6(friends);
+*/
