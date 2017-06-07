d95885e33a5170aa4d554d7def287cddc579df0a
Sami Peralahti <sami.peralahti@gmail.com>
2017-06-07 20:48:49 +0300

    spread operator

--- moreJS/7-EcmaScript2015/script.js
+++ moreJS/7-EcmaScript2015/script.js
@@ -306,6 +306,7 @@ for(var i = 0;i < boxesArr5.length;i++){
   }
 */
 // ES5
+/*
 var ages = [12,17,8,21,14,11];
 
 var full = ages.map(function(el){
@@ -318,3 +319,36 @@ console.log(ages[full.indexOf(true)]);
 
 console.log(ages.findIndex(cur => cur >= 18));
 console.log(ages.find(cur => cur >= 18));
+*/
+
+// Spread Operator
+/*
+function addFourAges(a,b,c,d){
+  return a+b+c+d;
+}
+
+var sum1 = addFourAges(18,30,12,21);
+console.log(sum1);
+
+//ES5
+var  ages = [18,30,12,21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+
+//ES6 ... = spread operator expands the arrays components
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+const familySmith = ["john", "jane","mark"];
+const familyMiller = ["marn", "kappa","zubraza"];
+
+const bigFamily = [...familySmith,"Lily",...familyMiller];
+console.log(bigFamily);
+
+const h = document.querySelector("h1");
+const boxes = document.querySelectorAll(".box");
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = "purple");
+*/
