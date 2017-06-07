373a379e10d7459d047b2719c7e26d0f7b4985f5
Sami Peralahti <sami.peralahti@gmail.com>
2017-06-07 21:10:33 +0300

    rest parameters es6

--- moreJS/7-EcmaScript2015/script.js
+++ moreJS/7-EcmaScript2015/script.js
@@ -352,3 +352,51 @@ const all = [h, ...boxes];
 
 Array.from(all).forEach(cur => cur.style.color = "purple");
 */
+
+//Rest parameters
+
+//ES5
+/*
+function isFullAge5(){
+  //console.log(arguments);
+  var argrArr = Array.prototype.slice.call(arguments);
+
+  argrArr.forEach(function(cur){
+    console.log((2016 - cur) >= 18);
+  });
+}
+
+isFullAge5(1990,1999,1965,2016,1987);
+*/
+/*
+//ES6
+
+function isFullAge6(...years){
+  years.forEach(cur => console.log((2016 - cur) >= 18));
+}
+
+isFullAge6(1990,1999,1965,2016,1987);
+*/
+/*
+
+function isFullAge5(limit){
+  //console.log(arguments);
+  //exludes the first argument
+  var argsArr = Array.prototype.slice.call(arguments, 1);
+
+  argsArr.forEach(function(cur){
+    console.log((2016 - cur) >= limit);
+  });
+}
+
+//isFullAge5(16,1990,1999,1965,2016,1987);
+
+
+//ES6
+
+function isFullAge6(limit,...years){
+  years.forEach(cur => console.log((2016 - cur) >= limit));
+}
+
+isFullAge6(16,1990,1999,1965,2016,1987);
+*/
