156ba7c76b1554f1dcc6cc9cc3f71874c5a6f87c
Sami Peralahti <sami.peralahti@gmail.com>
2017-06-07 21:20:05 +0300

    default parameters

--- moreJS/7-EcmaScript2015/script.js
+++ moreJS/7-EcmaScript2015/script.js
@@ -400,3 +400,38 @@ function isFullAge6(limit,...years){
 
 isFullAge6(16,1990,1999,1965,2016,1987);
 */
+
+// Default parameters
+
+//ES5
+/*
+function SmithPerson(firstName, yearOfBirth, lastName, nationality){
+
+  lastName === undefined ? lastName = "Smith": lastName = lastName;
+  nationality === undefined ? nationality = "american": nationality = nationality;
+
+}
+this.firstName = firstName;
+this.lastName = lastName;
+this.yearOfBirth = yearOfBirth;
+this.nationality = nationality;
+
+var john = new SmithPerson("John",1990);
+console.log(john);
+var emily = new SmithPerson("Emily", 1983, "Diaz", "Spansis");
+console.log(emily);
+*/
+
+//ES6
+/*
+function SmithPerson(firstName, yearOfBirth, lastName = "Smith", nationality = "american"){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.yearOfBirth = yearOfBirth;
+  this.nationality = nationality;
+}
+var john = new SmithPerson("John",1990);
+console.log(john);
+var emily = new SmithPerson("Emily", 1983, "Diaz", "Spansish");
+console.log(emily);
+*/
